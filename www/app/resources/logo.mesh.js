725a5879d65418ac6cd6a7d056362e6619b3fd6e
Ben Harling <bharling@doner.co.uk>
2014-03-11 17:32:35 +0000

    make logo shiny

--- www/app/resources/logo.mesh.js
+++ www/app/resources/logo.mesh.js
@@ -26,7 +26,7 @@
 		"colorSpecular" : [1.0, 1.0, 1.0],
 		"depthTest" : true,
 		"depthWrite" : true,
-		"shading" : "Lambert",
+		"shading" : "Phong",
 		"specularCoef" : 59,
 		"transparency" : 1.0,
 		"transparent" : false,
